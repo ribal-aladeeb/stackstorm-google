51d2bf5fce911a172146685c88d1767a7fa4520e
Ribal Aladeeb <raladeeb@drw.com>
2022-10-21 11:03:30 -0400

    fix: related search exception thrown
    Still not clear why related searches are of nonetype (looks like the underlying
    lib has a bug).

--- actions/get_search_results.py
+++ actions/get_search_results.py
@@ -5,6 +5,14 @@ from st2common.runners.base_action import Action
 
 class GetSearchResultsAction(Action):
     def run(self, query, count=10):
+
         srch = Search(query.lower(), number_of_results=count)
-        srch.load()
-        return (True, {"result": srch.as_dict()})
+        try:
+            srch.load()
+            mapped = srch.as_dict()
+
+        except RelatedSearchError:
+            srch._related_searches = []
+            mapped = srch.as_dict()
+
+        return (True, {"result": mapped})
